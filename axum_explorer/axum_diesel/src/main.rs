a923b45e7d473f544cc1f4980d21b34c6fa22def
insightbuilder <kamaljp@gmail.com>
2024-09-25 17:52:00 +0530

    added cors layer to axum_diesel

--- axum_explorer/axum_diesel/src/main.rs
+++ axum_explorer/axum_diesel/src/main.rs
@@ -9,6 +9,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use std::error::Error;
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -17,7 +18,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/all", get(show_employees))
         .route("/show/:sid", get(show_employee))
         .route("/rm/:pid", delete(delete_employee))
-        .route("/make", post(create_employee));
+        .route("/make", post(create_employee))
+        // following is for allowing access inside same machine servers
+        .layer(CorsLayer::new().allow_origin(Any).allow_methods(Any));
 
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
     axum::serve(listener, app).await?;
