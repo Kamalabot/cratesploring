c1323ec73999209e3953db3d7d084dd82f80b9a1
aicoder <kamalabot@github.com>
2024-10-01 08:09:26 +0000

    start kalosm intro

--- floneum_explorer/next-gen-ai/src/main.rs
+++ floneum_explorer/next-gen-ai/src/main.rs
@@ -2,12 +2,23 @@
 #![allow(unused_imports)]
 
 use std::io::Write;
+use std::io::stdin;
 use kalosm::{*, language::*};
 
 #[tokio::main]
 async fn main() {
     let mut llm = Llama::new().await.unwrap();
-    let prompt = "The following is 100 words essay on rust ";
-    let mut stream = llm.stream_text(prompt).with_max_length(1000).await.unwrap();
-    stream.to_std_out().await.unwrap();
+    let mut prompt = String::new(); 
+    loop {
+
+        println!("Type exit or else");
+        println!("Ask your question: ");
+        stdin().read_line(&mut prompt).unwrap();
+        if prompt.trim() == "exit"{
+            break
+        }
+        println!("The LLM reply: ");
+        let mut stream = llm.stream_text(&prompt).with_max_length(250).await.unwrap();
+        stream.to_std_out().await.unwrap();
+    }
 }
