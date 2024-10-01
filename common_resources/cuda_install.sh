141761584bed9191bdef916eab1902225ba3d075
insightbuilder <kamaljp@gmail.com>
2024-10-01 11:19:27 +0530

    completed the ec2 scripts

--- common_resources/cuda_install.sh
+++ common_resources/cuda_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "starting cuda12-2 install"
-
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda-repo-ubuntu2204-12-2-local_12.2.0-535.54.03-1_amd64.deb
@@ -9,6 +8,9 @@ sudo dpkg -i cuda-repo-ubuntu2204-12-2-local_12.2.0-535.54.03-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update -y
 sudo apt-get -y install cuda-toolkit-12-2 
+echo 'export LD_LIBRARY_PATH="/usr/local/cuda-12.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"' >> ~/.bashrc
+echo 'export PATH="/usr/local/cuda-12.2/bin${PATH:+:${PATH}}"' >> ~/.bashrc
+source ~/.bashrc 
 echo "Completed cuda installation"
 
 echo "Starting cudnn install"
@@ -17,5 +19,15 @@ sudo dpkg -i cudnn-local-repo-ubuntu2204-9.4.0_1.0-1_amd64.deb
 sudo cp /var/cudnn-local-repo-ubuntu2204-9.4.0/cudnn-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update -y
 sudo apt-get -y install cudnn-cuda-12
-echo 'export CUDNN_LIB=/usr/lib/x86-64-linux-gnu >> ~/.bashrc'
+echo 'export CUDNN_LIB=/usr/lib/x86-64-linux-gnu' >> ~/.bashrc
 echo "Completed cudnn installation"
+
+echo "Ensure AWS Credentials are updated in .bashrc"
+source ~/.bashrc
+echo "Installing Nvidia Driver"
+aws s3 cp s3://ec2-linux-nvidia-drivers/grid-16.6/NVIDIA-Linux-x86_64-535.183.01-grid-aws.run .
+sudo chmod +x NVIDIA-Linux-x86_64*.run
+sudo /bin/sh ./NVIDIA-Linux-x86_64*.run
+echo "Completed driver install"
+
+
--- common_resources/ec2_script.sh
+++ common_resources/ec2_script.sh
@@ -2,18 +2,24 @@
 
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y build-essential 
-sudo apt install - y unzip npm libfuse2 python3-pip fontconfig python3.12-venv xclip xsel
+sudo apt install - y unzip npm python3-pip fontconfig python3.12-venv xclip xsel
+sudo apt install -y openssl libssl-dev libavformat-dev libavfilter-dev libavdevice-dev ffmpeg tesseract-ocr libtesseract-dev libasound2-dev cmake libxcb1-dev
+sudo apt install -y libclang-dev librocksdb-dev awscli python3-pip
+echo "adding additional libraries including awscli"
+sudo apt-get upgrade -y linux-aws
+sudo apt-get install -y linux-headers-$(uname -r)
 echo "installing nvim"
 wget https://github.com/neovim/neovim-releases/releases/download/v0.10.1/nvim.appimage
 sudo mkdir /opt/nvim
 sudo mv nvim.appimage /opt/nvim/nvim
 sudo chmod u+x /opt/nvim/nvim
 sudo echo 'export PATH="$PATH:/opt/nvim"' >> ~/.bashrc
-echo "added nvim to path"
-echo "***********************************************"
-sudo apt install -y openssl libssl-dev libavformat-dev libavfilter-dev libavdevice-dev ffmpeg tesseract-ocr libtesseract-dev libasound2-dev cmake libxcb1-dev
-sudo apt-get upgrade -y linux-aws
-sudo apt-get install -y linux-headers-$(uname -r)
+echo "added nvim to path. Installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+rustup component add rust-analyzer 
+rustup component add clippy
+rustup component add rustfmt 
+echo "installed rust and components"
 cat << EOF | sudo tee --append /etc/modprobe.d/blacklist.conf
 blacklist vga16fb
 blacklist nouveau
@@ -25,15 +31,6 @@ echo "blacklist entries added"
 echo 'GRUB_CMDLINE_LINUX="rdblacklist=nouveau"' >> /etc/default/grub
 sudo update-grub
 echo "updated grub"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-rustup component add rust-analyzer 
-rustup component add clippy
-rustup component add rustfmt 
-echo "installed rust and components"
-echo "***********************************************"
-curl -sSf https://install.surrealdb.com | sh
-echo "adding additional libraries including awscli"
-sudo apt install -y libclang-dev librocksdb-dev awscli python3-pip
 echo "Update .bashrc file"
 echo 'export AWS_ACCESS_KEY_ID="your-access-key-id"' >> ~/.bashrc
 echo 'export AWS_SECRET_ACCESS_KEY="your-secret-access-key"' >> ~/.bashrc
@@ -44,10 +41,18 @@ echo "installing lvim"
 wget https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh
 sudo chmod +x install.sh
 source ~/.bashrc
-sudo ./install.sh -y
-LV_BRANCH='release-1.4/neovim-0.9' install.sh -y
+sudo apt install -y libfuse2 
+./install.sh -y
 sudo echo 'export PATH="$PATH:/home/ubuntu/.local/bin"' >> ~/.bashrc
 sudo echo 'alias cls="clear"' >> ~/.bashrc
 source ~/.bashrc
 echo "Lvim Install completed"
+echo "Installing Surreal DB completed"
+curl -sSf https://install.surrealdb.com | sh
+echo "Installing Vnc & Ubuntu Desktop"
+sudo apt-get install -y lightdm ubuntu-desktop
+sudo apt install -y tightvncserver
+sudo apt install -y gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+echo "Desktop installed, now Configure the vnc server xstartup"
 echo "visit https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/install-nvidia-driver.html for next steps"
+vncserver :1
