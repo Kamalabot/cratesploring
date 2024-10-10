bbc0832f46d79996ad82b2874c75a5eedfa62da4
insightbuilder <kamaljp@gmail.com>
2024-10-10 18:46:46 +0530

    updated onnx

--- candle_explorer/onnx-inference/src/main.rs
+++ candle_explorer/onnx-inference/src/main.rs
@@ -4,14 +4,14 @@ use clap::{Parser, ValueEnum};
 #[derive(Clone, Copy, Debug, ValueEnum)]
 enum Which {
     SqueezeNet,
-    EfficientNet,
+    EfficientN,
 }
 
 #[derive(Parser)]
 struct Args {
     #[arg(long)]
     image: String,
-
+    /// provide the full path of model.onnx
     #[arg(long)]
     model: Option<String>,
 
@@ -22,36 +22,47 @@ struct Args {
 
 pub fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    // loading image from the given path
     let image = candle_examples::imagenet::load_image224(args.image)?;
+    // depending on model chosen updating the image tensor
     let image = match args.which {
         Which::SqueezeNet => image,
-        Which::EfficientNet => image.permute((1, 2, 0))?,
+        Which::EfficientN => image.permute((1, 2, 0))?,
     };
 
     println!("loaded image {image:?}");
-
+    // try getting the model from which / model parameter
     let model = match args.model {
+        // below is model.onnx file extension
         Some(model) => std::path::PathBuf::from(model),
         None => match args.which {
+            // proceed to download the model
             Which::SqueezeNet => hf_hub::api::sync::Api::new()?
                 .model("lmz/candle-onnx".into())
                 .get("squeezenet1.1-7.onnx")?,
-            Which::EfficientNet => hf_hub::api::sync::Api::new()?
+            Which::EfficientN => hf_hub::api::sync::Api::new()?
                 .model("onnx/EfficientNet-Lite4".into())
                 .get("efficientnet-lite4-11.onnx")?,
         },
     };
-
+    // read the model into onnx
     let model = candle_onnx::read_file(model)?;
+    // create graph instance
     let graph = model.graph.as_ref().unwrap();
+    // mutable inputs hashmap
     let mut inputs = std::collections::HashMap::new();
+    // creating the inputs with string and image tensor from above
     inputs.insert(graph.input[0].name.to_string(), image.unsqueeze(0)?);
+    // running the simple_eval function with model & inputs
     let mut outputs = candle_onnx::simple_eval(&model, inputs)?;
+    // extracting the output tensor, and discarding string
     let output = outputs.remove(&graph.output[0].name).unwrap();
+    // get the probabilities
     let prs = match args.which {
         Which::SqueezeNet => candle_nn::ops::softmax(&output, D::Minus1)?,
-        Which::EfficientNet => output,
+        Which::EfficientN => output,
     };
+    // to vectors
     let prs = prs.i(0)?.to_vec1::<f32>()?;
 
     // Sort the predictions and take the top 5
@@ -63,6 +74,7 @@ pub fn main() -> anyhow::Result<()> {
     for &(i, p) in &top {
         println!(
             "{:50}: {:.2}%",
+            // here the imagenet is containing the classes
             candle_examples::imagenet::CLASSES[i],
             p * 100.0
         );
